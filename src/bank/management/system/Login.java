18e402a894f5bf19f990324b2163cba2cffc1521
Vg119 <90823605+Vg119@users.noreply.github.com>
2023-07-02 02:10:52 +0530

    DAY2

---
+++ src/bank/management/system/Login.java
@@ -0,0 +1,149 @@
+//LOGIN PAGE
+package bank.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+//Jframe so as to show a frame,actionslistener to listen to events
+public class Login extends JFrame implements ActionListener {
+    JLabel label1,label2,label3; //for labels
+    JTextField textField2; //for textfields
+    JPasswordField passwordField3;//for passwordfields
+
+    JButton button1,button2,button3; //for buttons
+    Login()
+    {
+        super("Vergab's Bank Management System");
+
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("./icons/bank.png")); //to set image on login page ,
+        Image i2 = i1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);//scaling image
+        ImageIcon i3 = new ImageIcon(i2); //to make image visible on frame we reconvert it
+        JLabel image = new JLabel(i3); //displaying image as label
+        image.setBounds(350,10,100,100);//where the image appears
+        add(image);//adding image on frame
+
+        ImageIcon ii1 = new ImageIcon(ClassLoader.getSystemResource("./icons/card.png")); //to set image on login page ,
+        Image ii2 = ii1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);//scaling image
+        ImageIcon ii3 = new ImageIcon(ii2); //to make image visible on frame
+        JLabel iimage = new JLabel(ii3); //displaying image as label
+        iimage.setBounds(635,350,100,100);//where the image appears
+        add(iimage);//adding image on screen
+
+        //setting label of text WELCOME...ATM.
+        label1 = new JLabel("WELCOME TO MY ATM");//main work of jlabel is to show text on screen
+        label1.setForeground(Color.WHITE);//setting colour white
+        label1.setFont(new Font("AvantGarde",Font.BOLD,38));//setting font of text
+        label1.setBounds(200,125,450,40); //distance from x ,y of frame not from screen.
+        add(label1);//adding to frame
+
+        //CARD NO
+        label2 = new JLabel("Card no.");
+        label2.setFont(new Font("Ralway",Font.BOLD,28));
+        label2.setForeground(Color.WHITE);
+        label2.setBounds(150,190,375,30);
+        add(label2);
+
+        //for user to enter text in textfield
+        textField2 = new JTextField(15);
+        textField2.setBounds(325,190,230,30);//y is same as it appears with card no
+        textField2.setFont(new Font("Arial",Font.BOLD,14));
+        add(textField2);
+
+
+        //PIN
+        label3 = new JLabel("PIN: ");
+        label3.setFont(new Font("Ralway",Font.BOLD,28));
+        label3.setForeground(Color.WHITE);
+        label3.setBounds(150,250,375,30);
+        add(label3);
+
+
+        //setting so user may enter password
+        passwordField3 = new JPasswordField(15);
+        passwordField3.setBounds(325,250,230,30);
+        passwordField3.setFont(new Font("Arial",Font.BOLD,14));
+        add(passwordField3);
+
+        //sign in button
+        button1 = new JButton("SIGN IN");
+        button1.setFont(new Font("Arial",Font.BOLD,14));
+        button1.setForeground(Color.WHITE);
+        button1.setBackground(Color.black); //background colour of button
+        button1.setBounds(300,300,100,30);
+        button1.addActionListener(this);//whenever a button is clicked , this listener listens and tells the Actionevent e that is pased to actionPerformed()
+        add(button1);
+
+        //clear button
+        button2 = new JButton("CLEAR");
+        button2.setFont(new Font("Arial",Font.BOLD,14));
+        button2.setForeground(Color.WHITE);
+        button2.setBackground(Color.black); //background colour of button
+        button2.setBounds(430,300,100,30);
+        button2.addActionListener(this);
+        add(button2);
+
+        //sign up button
+        button3 = new JButton("SIGN UP");
+        button3.setFont(new Font("Arial",Font.BOLD,14));
+        button3.setForeground(Color.WHITE);
+        button3.setBackground(Color.black); //background colour of button
+        button3.setBounds(300,350,230,30);
+        button3.addActionListener(this);
+        add(button3);
+
+        //NOTE :- THE POSITIONS OF ELEMENTS R SET BY HIT & TRIAL
+
+
+
+
+        //setting background,setting it after setting all images as this is background
+        ImageIcon iii1 = new ImageIcon(ClassLoader.getSystemResource("./icons/backbg.png")); //to set image on login page ,
+        Image iii2 = iii1.getImage().getScaledInstance(850,4800,Image.SCALE_DEFAULT);//scaling image
+        ImageIcon iii3 = new ImageIcon(iii2); //to make image visible on frame
+        JLabel iiimage = new JLabel(iii3); //displaying image as label
+        iiimage.setBounds(0,0,850,480);//where the image appears
+        add(iiimage);//adding image on screen
+
+
+
+        setLayout(null);//setting default layout null
+        setSize(850,480); // setting size of frame
+        setLocation(450,200);//so that frame opens at 450 from left x axis & 200 from top (y axis)
+        setVisible(true);//setting true so as to make frame visible, always keep setvisibility after setting whatever u want to be visible
+
+    }
+
+    //method to be overrided so as to implement actionslistener
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        try {  //there r many errors here hence we use try catch
+
+            if(e.getSource()==button1)//if e notifies that action is performed on button1
+            {
+
+
+            } else if (e.getSource()==button2) {//if e notifies that action is performed on button2
+
+                textField2.setText("");//setting text & password fields as empty strings,so that on pressing CLEAR button these fields become empty
+                passwordField3.setText("");
+            } else if (e.getSource()==button3) {//if e notifies that action is performed on button3
+
+            }
+        }catch (Exception E)
+        {
+            E.printStackTrace();
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        new Login();
+
+    }
+}
---
+++ src/bank/management/system/Signup.java
@@ -0,0 +1,234 @@
+//SIGNUP CLASS
+package bank.management.system;
+
+import com.toedter.calendar.JDateChooser;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class Signup extends JFrame implements ActionListener {
+    JButton next;
+    JRadioButton r1,r2,m1,m2,m3; //radiobutton
+
+    JTextField textName,textFname,textEmail,textAdd,textCity,textPin,textState;//for taking name as i/p
+
+    JDateChooser dateChooser; //for choosing dates by opening calender ; downloaded external jar file for it.
+    Random ran = new Random();//util.random for random application no.
+    long first4 = (ran.nextLong()%9000L)+1000L;//to keep max as 9999(4 digit) , coz divide by 9000 gives <9000 and add 1000
+
+    String first = " " + Math.abs(first4);
+    Signup()
+    {
+        super("VERGAB'S APPLICATION FORM");
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("./icons/bank.png")); //to set image on login page ,
+        Image i2 = i1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);//scaling image
+        ImageIcon i3 = new ImageIcon(i2); //to make image visible on frame we reconvert it
+        JLabel image = new JLabel(i3); //displaying image as label
+        image.setBounds(25,10,100,100);//where the image appears
+        add(image);//adding image on frame
+
+        //APPL. NO
+        JLabel label1 = new JLabel("APPLICATION FORM NO." +first);
+        label1.setBounds(160,20,600,40);
+        label1.setFont(new Font("Raleway",Font.BOLD,38));
+        add(label1);
+
+
+        //PAGE 1
+        JLabel label2 = new JLabel("Page 1");
+        label2.setBounds(380,70,600,30);
+        label2.setFont(new Font("Raleway",Font.BOLD,22));
+        add(label2);
+
+        JLabel label3 = new JLabel("Personal Details");
+        label3.setBounds(315,90,600,40);
+        label3.setFont(new Font("Raleway",Font.BOLD,25));
+        add(label3);
+
+        //NAME
+        JLabel labelName = new JLabel("Name : ");
+        labelName.setFont(new Font("Raleway",Font.BOLD,20));
+        labelName.setBounds(100,190,100,30);
+        add(labelName);
+
+
+        //NAME TEXTFIELD
+        textName = new JTextField();
+        textName.setFont(new Font("Raleway",Font.BOLD,14));
+        textName.setBounds(300,190,400,30);
+        add(textName);
+
+        // FATHER'S NAME
+        JLabel labelFName = new JLabel("Father's Name : ");
+        labelFName.setFont(new Font("Raleway",Font.BOLD,20));
+        labelFName.setBounds(100,240,200,35);
+        add(labelFName);
+
+
+        // FATHER NAME TEXTFIELD
+        textFname = new JTextField();
+        textFname.setFont(new Font("Raleway",Font.BOLD,14));
+        textFname.setBounds(300,240,400,30);
+        add(textFname);
+
+        //DOB
+        JLabel dob = new JLabel("Date of Birth : ");
+        dob.setFont(new Font("Raleway",Font.BOLD,20));
+        dob.setBounds(100,340,200,35);
+        add(dob);
+
+        //opens up calender
+        dateChooser = new JDateChooser();
+        dateChooser.setForeground(new Color(105,105,105));
+        dateChooser.setBounds(300,340,400,30);
+        add(dateChooser);
+
+       //GENDER
+        JLabel labelG  = new JLabel("Gender : ");
+        labelG.setFont(new Font("Raleway",Font.BOLD,20));
+        labelG.setBounds(100,290,200,30);
+        add(labelG);
+
+        //RADIOBUTTONS
+        r1 = new JRadioButton("Male");
+        r1.setFont(new Font("Raleway",Font.BOLD,14));
+        r1.setBounds(300,290,60,30);
+        r1.setBackground(new Color(222,255,228));
+        add(r1);
+
+        r2 = new JRadioButton("Female");
+        r2.setFont(new Font("Raleway",Font.BOLD,14));
+        r2.setBounds(450,290,90,30);
+        r2.setBackground(new Color(222,255,228));
+        add(r2);
+
+        //we r adding the 2 buttons to a button group because we only want one  of them to be selected at a time
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(r1);
+        buttonGroup.add(r2);
+
+        //EMAIL
+        JLabel labelemail = new JLabel("Email Address : ");
+        labelemail.setFont(new Font("Raleway",Font.BOLD,20));
+        labelemail.setBounds(100,390,200,30);
+        add(labelemail);
+
+
+        textEmail = new JTextField();
+        textEmail.setFont(new Font("Raleway",Font.BOLD,14));
+        textEmail.setBounds(300,390,400,30);
+        add(textEmail);
+
+        //MARRAIGE STATUS
+        JLabel labelms = new JLabel("Marital Status : ");
+        labelms.setFont(new Font("Raleway",Font.BOLD,20));
+        labelms.setBounds(100,440,200,30);
+        add(labelms);
+
+        //RADIOBUTTONS
+        m1 = new JRadioButton("Married");
+        m1.setFont(new Font("Raleway",Font.BOLD,14));
+        m1.setBounds(300,440,100,30);
+        m1.setBackground(new Color(222,255,228));
+        add(m1);
+
+        m2 = new JRadioButton("Unmarried");
+        m2.setFont(new Font("Raleway",Font.BOLD,14));
+        m2.setBounds(450,440,100,30);
+        m2.setBackground(new Color(222,255,228));
+        add(m2);
+
+        m3 = new JRadioButton("Other");
+        m3.setFont(new Font("Raleway",Font.BOLD,14));
+        m3.setBounds(600,440,100,30);
+        m3.setBackground(new Color(222,255,228));
+        add(m3);
+
+        ButtonGroup buttonGroup1 = new ButtonGroup();
+        buttonGroup1.add(m1);
+        buttonGroup1.add(m2);
+        buttonGroup1.add(m3);
+
+
+
+
+        //ADDRESS
+        JLabel labelADD = new JLabel("Address : ");
+        labelADD.setFont(new Font("Raleway",Font.BOLD,20));
+        labelADD.setBounds(100,490,200,30);
+        add(labelADD);
+
+
+        textAdd = new JTextField();
+        textAdd.setFont(new Font("Raleway",Font.BOLD,14));
+        textAdd.setBounds(300,490,400,30);
+        add(textAdd);
+
+        //CITY
+        JLabel labelCity = new JLabel("City : ");
+        labelCity.setFont(new Font("Raleway",Font.BOLD,20));
+        labelCity.setBounds(100,540,200,30);
+        add(labelCity);
+
+
+        textCity = new JTextField();
+        textCity.setFont(new Font("Raleway",Font.BOLD,14));
+        textCity.setBounds(300,540,400,30);
+        add(textCity);
+
+        //PINCODE
+        JLabel labelPin = new JLabel("Pin : ");
+        labelPin.setFont(new Font("Raleway",Font.BOLD,20));
+        labelPin.setBounds(100,590,200,30);
+        add(labelPin);
+
+
+        textPin = new JTextField();
+        textPin.setFont(new Font("Raleway",Font.BOLD,14));
+        textPin.setBounds(300,590,400,30);
+        add(textPin);
+
+        //STATE
+        JLabel labelState = new JLabel("State : ");
+        labelState.setFont(new Font("Raleway",Font.BOLD,20));
+        labelState.setBounds(100,640,200,30);
+        add(labelState);
+
+
+        textState = new JTextField();
+        textState.setFont(new Font("Raleway",Font.BOLD,14));
+        textState.setBounds(300,640,400,30);
+        add(textState);
+
+
+
+        next = new JButton("Next");
+        next.setFont(new Font("Raleway",Font.BOLD,14));
+        next.setBackground(Color.BLACK); //background colour white
+        next.setForeground(Color.WHITE); //text colour white
+        next.setBounds(620,710,80,30);
+        next.addActionListener(this);
+        add(next);
+
+
+        getContentPane().setBackground(new Color(222,255,228)); //setting background
+        setLayout(null);
+        setSize(850,800);//frame size
+        setLocation(360,40);
+        setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+        new Signup();
+    }
+}
